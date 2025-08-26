258ecd4eea069522126e02fe06646200bea44e52
Nikolas <nikmarlon.santos@gmail.com>
2025-08-26 11:40:11 -0300

    Componente de histórico de operações

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Button } from "./components/Button";
 import { Calculator } from "./components/Calculator";
 import { Card } from "./components/Card";
+import { OperationHistory } from "./components/OperationHistory";
 import { Text } from "./components/Text";
 
 export function App() {
   return (
-    <main className="py-28 px-4">
+    <main className="py-28 px-4 flex gap-4">
       <Calculator />
+      <OperationHistory />
     </main>
   )
 }
---
+++ src/components/OperationHistory.jsx
@@ -0,0 +1,18 @@
+import { Card } from "./Card";
+import { Text } from "./Text";
+
+
+export function OperationHistory(){
+    return(
+        <Card className='py-10 px-8'>
+            <Text as="h1" variant="heading" className='mb-4'>Histórico de operações</Text>
+
+            <ul className="flex flex-col gap-3">
+                <Text as="li">1 + 1 = 2</Text>
+                <Text as="li">1 + 1 + 4 = 6</Text>
+                <Text as="li">1 + 1 = 2</Text>
+            </ul>
+
+        </Card>
+    )
+}
