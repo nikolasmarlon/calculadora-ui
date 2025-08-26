9e09c6b741a7976c68ae691e38e165fdf6416550
Nikolas <nikmarlon.santos@gmail.com>
2025-08-26 12:32:32 -0300

    Finalizando histórico e layout geral

--- src/components/OperationHistory.jsx
+++ src/components/OperationHistory.jsx
@@ -4,7 +4,7 @@ import { Text } from "./Text";
 
 export function OperationHistory(){
     return(
-        <Card className='py-10 px-8'>
+        <Card className='py-10 px-8 w-full'>
             <Text as="h1" variant="heading" className='mb-4'>Histórico de operações</Text>
 
             <ul className="flex flex-col gap-3">
