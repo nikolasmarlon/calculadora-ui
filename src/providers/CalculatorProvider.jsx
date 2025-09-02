4a986ff3d4213877a86d72efe781b0329501af3a
Nikolas <nikmarlon.santos@gmail.com>
2025-09-02 17:06:33 -0300

    useEffect e localstorage

--- src/providers/CalculatorProvider.jsx
+++ src/providers/CalculatorProvider.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CalculatorContext } from "../contexts/CalculatorContext";
 
 
 export function CalculatorProvider({children}){
 
     const [ history, setHistory ] = useState([]);
+    const historyStoragekey = 'history'
+
+
+    useEffect(()=>{
+        const historyFromStorage = localStorage.getItem(historyStoragekey)
+        setHistory(JSON.parse(historyFromStorage || '[]'))
+    }, [])
 
     function updateHistory(operation, parsedResult){
-        setHistory((prev) => [...prev, `${operation}=${parsedResult}`])
+        setHistory((prev) => {
+            const updatedHistory = [...prev, `${operation}=${parsedResult}`]
+            localStorage.setItem(historyStoragekey, JSON.stringify(updatedHistory))
+            return updatedHistory
+        })
     }
 
     return(
